a6699c8cbe1dc47d09dc1d095f4de542d6657ea1
Gislane Natália de Souza Miranda <gislane.natalia.souza.miranda@gmail.com>
2022-09-20 08:01:51 -0300

    Req 12 - data corrigida e Req 13 e 14

--- desafio12.sql
+++ desafio12.sql
@@ -1,3 +1,3 @@
 -- Active: 1626983563328@@localhost@3306@northwind
 SELECT submitted_date FROM northwind.purchase_orders
-WHERE DATE(submitted_date) = '26-04-2006';
+WHERE DATE(submitted_date) = '2006-04-26';
