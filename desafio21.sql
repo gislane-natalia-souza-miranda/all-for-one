6bd4998f81c465135f775f7cd7a1836f1300f21f
Gislane Natália de Souza Miranda <gislane.natalia.souza.miranda@gmail.com>
2022-09-20 16:11:57 -0300

    Req 21 e 22

--- desafio21.sql
+++ desafio21.sql
@@ -1,3 +1,4 @@
+-- Active: 1626983563328@@localhost@3306@northwind
 INSERT INTO northwind.order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id )
 VALUES(69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129),
 (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
