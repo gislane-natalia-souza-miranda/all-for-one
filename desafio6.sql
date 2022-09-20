2d36d7e930ac96046cc240eaebe18844c979a7ae
Gislane Natália de Souza Miranda <gislane.natalia.souza.miranda@gmail.com>
2022-09-20 06:49:39 -0300

    Req 6 e 7

---
+++ desafio6.sql
@@ -0,0 +1,2 @@
+-- Active: 1626983563328@@localhost@3306@northwind
+SELECT product_name, id FROM northwind.products ORDER BY product_name ASC;
---
+++ desafio7.sql
@@ -0,0 +1,2 @@
+-- Active: 1626983563328@@localhost@3306@northwind
+SELECT id FROM northwind.products ORDER BY id ASC LIMIT 5 OFFSET 40;
