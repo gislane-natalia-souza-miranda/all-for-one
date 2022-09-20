093aa2ad3b08d9c881f4e0b251545a88e2d5ae9e
Gislane Natália de Souza Miranda <gislane.natalia.souza.miranda@gmail.com>
2022-09-20 14:57:11 -0300

    Req 15 - Nomeando coluna e Req 17 e 18

--- desafio15.sql
+++ desafio15.sql
@@ -1,2 +1,3 @@
 -- Active: 1626983563328@@localhost@3306@northwind
-SELECT HOUR(submitted_date) FROM northwind.purchase_orders;
+SELECT HOUR(submitted_date) AS submitted_hour
+FROM northwind.purchase_orders;
---
+++ desafio18.sql
@@ -0,0 +1,4 @@
+-- Active: 1626983563328@@localhost@3306@northwind
+SELECT * FROM northwind.purchase_orders
+WHERE supplier_id = 3 
+AND status_id = 2;
