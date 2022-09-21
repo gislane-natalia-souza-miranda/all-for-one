38ca221f0b4432d50787dfba72b9d10b91525fdd
Gislane Natália de Souza Miranda <gislane.natalia.souza.miranda@gmail.com>
2022-09-21 15:36:47 -0300

    Req 25, 26 e 27

---
+++ desafio26.sql
@@ -0,0 +1,3 @@
+-- Active: 1626983563328@@localhost@3306@northwind
+DELETE FROM northwind.order_details
+WHERE unit_price > 10.0000;
---
+++ desafio27.sql
@@ -0,0 +1,2 @@
+-- Active: 1626983563328@@localhost@3306@northwind
+DELETE FROM northwind.order_details;
